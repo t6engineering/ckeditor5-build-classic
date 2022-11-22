38b298ed05055ee40983a6a05a9ba643b26ea6ee
Cuong Pham <cpham@talosix.com>
2022-11-22 13:56:07 +0700

    support html

--- build/translations/jv.js
+++
@@ -1,1 +0,0 @@
-!function(n){const a=n.jv=n.jv||{};a.dictionary=Object.assign(a.dictionary||{},{"%0 of %1":"%0 saking %1",Cancel:"Batal","Cannot upload file:":"Mboden saged ngirim berkas:","HTML object":"Obyek HTML",Save:"Rimat","Show more items":"Tampilaken langkung kathah"}),a.getPluralForm=function(n){return 0}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
